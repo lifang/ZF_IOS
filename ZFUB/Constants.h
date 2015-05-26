deaf845d673c0a6aa12d1c98b87886f06867f533
wufei <468008903@qq.com>
2015-05-26 17:18:56 +0800

    5.26

--- ZFUB/Constants.h
+++ ZFUB/Constants.h
@@ -47,7 +47,7 @@
 //#define kUnionPayURL  @"http://121.40.84.2:8080/ZFMerchant/unionpay.do" //测试
 
 #define kMode_Production             @"00"  //线上
-#define kUnionPayURL  @"http://121.40.64.167:8080/unionpay.do" //线上
+#define kUnionPayURL  @"http://www.ebank007.com/unionpay.do" //线上
 
 
 #define kImageName(name) [UIImage imageNamed:name]
